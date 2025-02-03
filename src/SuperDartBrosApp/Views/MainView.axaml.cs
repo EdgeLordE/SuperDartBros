0b503be7a3b768465ff212a3f17f11b3dfcfa9d8
Emil_Wagner <emil.wagner@student.htl-rankweil.at>
2025-02-03 08:42:17 +0100

    Projekt erstellt

---
+++ src/SuperDartBrosApp/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace SuperDartBrosApp.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ src/SuperDartBrosApp/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace SuperDartBrosApp.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
