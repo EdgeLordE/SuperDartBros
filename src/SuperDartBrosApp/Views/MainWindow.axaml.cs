582da2f87b0585ca0730e965c3d502d954b9497e
EdgeLordJ <joudi.khalil@student.htl-rankweil.at>
2025-02-03 13:29:23 +0100

    Anfang programmiert MainWindow und DartboardUserControl(MainView) und MessageBox erstellt weil es keine gibt

--- src/SuperDartBrosApp/Views/MainWindow.axaml.cs
+++ src/SuperDartBrosApp/Views/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 ﻿using Avalonia.Controls;
+using ReactiveUI;
 
 namespace SuperDartBrosApp.Views;
 
@@ -7,5 +8,19 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private async void  BtnStart_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        if (TBPlayer1.Text == "" || TBPlayer2.Text == "" || TBPlayer1.Text == " " || TBPlayer2.Text == " ")
+        {
+            MessageBox msgBox = new MessageBox("Hallo, das ist eine eigene MessageBox!");
+            await msgBox.ShowDialog(this);
+        }
+        else
+        {
+            UserControl userControl = new MainView();
+            Content = userControl;
+        }
     }
 }
