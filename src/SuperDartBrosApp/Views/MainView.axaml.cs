02796d8715830113279d5682113707e41013cc55
Emil_Wagner <emil.wagner@student.htl-rankweil.at>
2025-02-04 09:37:50 +0100

    Dartboard weiter

--- src/SuperDartBrosApp/Views/MainView.axaml.cs
+++ src/SuperDartBrosApp/Views/MainView.axaml.cs
@@ -1,4 +1,7 @@
-﻿using Avalonia.Controls;
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Shapes;
+using Avalonia.Media;
 
 namespace SuperDartBrosApp.Views;
 
@@ -7,9 +10,15 @@ public partial class MainView : UserControl
     public MainView()
     {
         InitializeComponent();
+        
     }
 
     private void Button_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
     {
+
     }
+
+    
+    
+
 }
