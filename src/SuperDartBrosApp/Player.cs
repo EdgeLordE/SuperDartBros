a4a051a473f44f4e92f907be7d5c5585e7910a20
Mathias Florea <mathias.florea@student.htl-rankweil.at>
2025-02-05 09:24:43 +0100

    Player

---
+++ src/SuperDartBrosApp/Player.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+
+namespace SuperDartBrosApp.Models
+{
+public class Player
+    {
+        public string Name { get; set; }
+        public int Score { get; set; }
+        
+        public List<string> Points = new List<string>();
+
+        public bool IsBot { get; set; }
+
+        public bool IsTextBoxEnabled { get; set; } = true;
+
+        public Player(string name, int score)
+        {
+            Name = name;
+            Score = score;
+        }
+
+        public void SubtractScore(int points)
+        {
+            if (Score - points >= 0)
+            {
+                Score -= points;
+            }
+        }
+
+        public void AddPoint(int points)
+        {
+            Points.Add(point);
+        }
+
+        public void ResetPoints()
+        {
+            Points.Clear();
+        }
+
+        public void Multiplier(int multiplier)
+        {
+
+            if(multiplier == 2)
+            {
+                Points.Add("D");
+            }
+            else if(multiplier == 3)
+            {
+                Points.Add("T");
+            }
+
+        }
+    }
+}
