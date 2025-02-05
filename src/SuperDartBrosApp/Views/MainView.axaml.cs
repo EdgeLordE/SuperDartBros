601badac8f0f8bc56cde670bb31210851ae576f1
EdgeLordJ <joudi.khalil@student.htl-rankweil.at>
2025-02-05 09:23:10 +0100

    Buttons Clicks gegeben

--- src/SuperDartBrosApp/Views/MainView.axaml.cs
+++ src/SuperDartBrosApp/Views/MainView.axaml.cs
@@ -10,10 +10,216 @@ public partial class MainView : UserControl
     public MainView()
     {
         InitializeComponent();
-        
     }
 
-    
+    private void Bull25_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        // Logik für Bull25
+    }
 
-    
+    private void BullE50_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        // Logik für BullE50
+    }
+
+    private void D20_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        // Logik für Double 20
+    }
+
+    private void D1_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        // Logik für Double 1
+    }
+
+    private void D18_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        // Logik für Double 18
+    }
+
+    private void D4_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        // Logik für Double 4
+    }
+
+    private void D13_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        // Logik für Double 13
+    }
+
+    private void D6_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        // Logik für Double 6
+    }
+
+    private void D10_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        // Logik für Double 10
+    }
+
+    private void D15_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        // Logik für Double 15
+    }
+
+    private void D2_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        // Logik für Double 2
+    }
+
+    private void D3_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        // Logik für Double 3
+    }
+
+    private void D5_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        // Logik für Double 5
+    }
+
+    private void D7_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        // Logik für Double 7
+    }
+
+    private void D8_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        // Logik für Double 8
+    }
+
+    private void D9_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        // Logik für Double 9
+    }
+
+    private void D11_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        // Logik für Double 11
+    }
+
+    private void D12_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        // Logik für Double 12
+    }
+
+    private void D14_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        // Logik für Double 14
+    }
+
+    private void D16_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        // Logik für Double 16
+    }
+
+    private void D17_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        // Logik für Double 17
+    }
+
+    private void D19_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        // Logik für Double 19
+    }
+
+    // Click-Events für Standard-Felder
+    private void S20_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        // Logik für Standard 20
+    }
+
+    private void S1_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        // Logik für Standard 1
+    }
+
+    private void S18_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        // Logik für Standard 18
+    }
+
+    private void S4_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        // Logik für Standard 4
+    }
+
+    private void S13_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        // Logik für Standard 13
+    }
+
+    private void S6_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        // Logik für Standard 6
+    }
+
+    private void S10_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        // Logik für Standard 10
+    }
+
+    private void S15_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        // Logik für Standard 15
+    }
+
+    private void S2_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        // Logik für Standard 2
+    }
+
+    private void S3_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        // Logik für Standard 3
+    }
+
+    private void S5_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        // Logik für Standard 5
+    }
+
+    private void S7_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        // Logik für Standard 7
+    }
+
+    private void S8_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        // Logik für Standard 8
+    }
+
+    private void S9_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        // Logik für Standard 9
+    }
+
+    private void S11_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        // Logik für Standard 11
+    }
+
+    private void S12_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        // Logik für Standard 12
+    }
+
+    private void S14_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        // Logik für Standard 14
+    }
+
+    private void S16_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        // Logik für Standard 16
+    }
+
+    private void S17_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        // Logik für Standard 17
+    }
+
+    private void S19_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        // Logik für Standard 19
+    }
 }
