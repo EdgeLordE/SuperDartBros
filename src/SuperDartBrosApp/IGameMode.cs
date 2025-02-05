288e3a40d4380c9ae53d4a6b4a4232088d99b73e
Mathias Florea <mathias.florea@student.htl-rankweil.at>
2025-02-05 16:23:57 +0100

    IGameMode X01

---
+++ src/SuperDartBrosApp/IGameMode.cs
@@ -0,0 +1,18 @@
+﻿using SuperDartBrosApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperDartBrosApp
+{
+    public interface IGameMode
+    {
+        void IntializeGameMode(IEnumerable<Player> players);
+        void ProcessThrow(Player player, string points);
+        bool IsGameOver();
+        
+        void GameFinished();
+    }
+}
---
+++ src/SuperDartBrosApp/X01.cs
@@ -0,0 +1,53 @@
+﻿using SuperDartBrosApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperDartBrosApp
+{
+    public class X01 : IGameMode
+    {
+        private int startPoints;
+        private IEnumerable<Player> players;
+
+
+        public X01(int startPoints)
+        {
+            this.startPoints = startPoints;
+        }
+
+        public void InitializeMode(IEnumerable<Player> players)
+        {
+            this.players = players;
+            foreach(var player in players)
+            {
+                player.Score = startPoints;
+            }
+        }
+
+        public void GameFinished()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void IntializeGameMode(IEnumerable<Player> players)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsGameOver()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ProcessThrow(Player player, string points)
+        {
+            throw new NotImplementedException();
+
+       
+        }
+
+    }
+}
